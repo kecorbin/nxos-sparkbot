08490dea366c9902ceca93de24119f1a47be375a
Matt Siegel <msiegy88@gmail.com>
2017-06-01 17:33:23 -0400

    Add files via upload

--- showint.py
+++ showint.py
@@ -1,32 +1,36 @@
 import requests
 import json
 
-url='http://10.10.20.58/ins'
-switchuser='admin'
-switchpassword='cisco123'
-port = 'eth1/1'
+def getint(portnum):
+    url='http://10.10.20.58/ins'
+    switchuser='admin'
+    switchpassword='cisco123'
+    portnum = str(portnum)
+    portnum = 'eth1/1'
+    myheaders={'content-type':'application/json-rpc'}
+    payload=[
+      {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+          "cmd": "show int " + portnum,
+          "version": 1
+        },
+        "id": 1
+      }
+    ]
+    response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+    portdetails = response['result']['body']['TABLE_interface']['ROW_interface']
 
-myheaders={'content-type':'application/json-rpc'}
-payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "show int " + port,
-      "version": 1
-    },
-    "id": 1
-  }
-]
-response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
 
-portdetails = response['result']['body']['TABLE_interface']['ROW_interface']
+    state = portdetails['admin_state'] + '/' + portdetails['state']
+    desc = portdetails['desc']
+    mode = portdetails['eth_mode']
+    speed = portdetails['eth_speed']
+    mtu = portdetails['eth_mtu']
+    mac = portdetails['eth_hw_addr']
 
+    return 'The state for port **' + portnum + '** is **' + state + '**!  Its description is **' + desc + \
+           '** and is currently enabled as a **' + mode + '** port running at **' + speed + '**'
 
-state = portdetails['admin_state'], '/', portdetails['state']
-desc = portdetails['desc']
-mode = portdetails['eth_mode']
-speed = portdetails['eth_speed']
-mtu = portdetails['eth_mtu']
-mac = portdetails['eth_hw_addr']
-
+#print getint('eth1/1')
