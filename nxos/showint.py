341dcd1ba39ef3d43f79a3340397d8f0c98a5e14
Matt Siegel <msiegy88@gmail.com>
2017-06-05 11:12:44 -0400

    Create showint.py
    initial commit

---
+++ nxos/showint.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+def getint(portnum, ip, user, passwd):
+    """
+    Get the show interface details for a port.
+    """
+    url = 'http://{}/ins'.format(ip)
+    switchuser = user
+    switchpassword = passwd
+    portnum = str(portnum)
+
+    myheaders = {'content-type':'application/json-rpc'}
+    payload = [
+      {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+          "cmd": "show int " + portnum,
+          "version": 1
+        },
+        "id": 1
+      }
+    ]
+    response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+
+    try:
+        portdetails = response['result']['body']['TABLE_interface']['ROW_interface']
+    except KeyError, e:
+        return 'Something went Wrong. Are you sure you typed the correct interface name?'
+    state = portdetails['admin_state'] + '/' + portdetails['state']
+    try:
+        desc = portdetails['desc']
+    except KeyError, e:
+        desc = "undefined"
+        pass
+    mode = portdetails['eth_mode']
+    speed = portdetails['eth_speed']
+    mtu = portdetails['eth_mtu']
+    mac = portdetails['eth_hw_addr']
+
+    return 'The state for port **' + portnum + '** is **' + state + '**!  Its description is **' + desc + \
+           '**. It is currently enabled as a **' + mode + '** port running at **' + speed + '**' + \
+           ' and MTU is set at **' + mtu + '**'
