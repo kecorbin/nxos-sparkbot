6f5d137c77dd513b805b79224cabdc324ebc3fe7
Matt Siegel <msiegy88@gmail.com>
2017-06-05 10:49:02 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,41 @@
 import os
 from ciscosparkbot import SparkBot
-from showint import getint
+from nxos.version import get_version
+from nxos.arp import get_iparp
+from nxos.showint import getint
 
-bot_email = "ENTERBOTUSER"
-spark_token = "ENTERTOKEN"
-bot_url = "ENTERURL"
-bot_app_name = "ENTERBOTNAME"
+bot_email = os.getenv("SPARK_BOT_EMAIL")
+spark_token = os.getenv("SPARK_BOT_TOKEN")
+bot_url = os.getenv("SPARK_BOT_URL")
+bot_app_name = os.getenv("SPARK_BOT_APP_NAME")
+nxos_username = os.getenv("NXOS_LOGIN")
+nxos_password = os.getenv("NXOS_PASSWORD")
 
-def do_something(incoming_msg):
-    return "i did what you said - {}".format(incoming_msg.text)
+def version(incoming_msg):
+    ip = incoming_msg.text.split()[-1]
+    version = get_version(ip, nxos_username, nxos_password)
+    return "Switch {} is running version {}".format(ip, version)
 
-bot = SparkBot(bot_app_name, spark_bot_token=spark_token,
-               spark_bot_url=bot_url, spark_bot_email=bot_email)
 
 def getinterface(spark_msg):
-    #print spark_msg.text
-    portname = spark_msg.text.split()[2]
-    return getint(portname)
+    ip = spark_msg.text.split()[2]
+    portname = spark_msg.text.split()[3]
+    return getint(portname, ip, nxos_username, nxos_password)
 
-bot.add_command('dosomething', 'help for do something', do_something)
-bot.add_command('port', 'usage: @nxosbot port Eth1/1', getinterface)
+bot = SparkBot(bot_app_name, spark_bot_token=spark_token,
+               spark_bot_url=bot_url, spark_bot_email=bot_email, debug=True)
+
+bot.add_command('version',
+                "Get the running version from an NX-OS switch. \
+                e.g `@{} version 10.1.1.1`".format(bot_email.split('@')[0]),
+                version)
+bot.add_command('arp',
+                'retrieve the arp table of a switch. \
+                e.g. `@{} arp 10.1.1.1`'.format(bot_email.split('@')[0]),
+                get_iparp)
+
+bot.add_command('port', 'retrieve interface details. \
+                e.g `@{} port 10.1.1.1 Eth1/1`'.format(bot_email.split('@')[0]),
+                getinterface)
+
 bot.run(host='0.0.0.0', port=5000)
